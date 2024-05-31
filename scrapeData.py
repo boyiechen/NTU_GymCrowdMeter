25790f7f757bfedaed3d607d3ac2ac1872d22e9c
boyiechen <boyie@example.com>
2024-05-31 08:47:45 +0100

    auto commit

--- scrapeData.py
+++ scrapeData.py
@@ -7,14 +7,15 @@ import datetime
 import os
 import sys
 from config import weather_url 
+from bs4 import BeautifulSoup
 
-class scraper(object):
+class Scraper:
 
     def __init__(self, weather_url = weather_url):
-        self.url = 'https://ntusportscenter.ntu.edu.tw/counter.txt'
+        self.url = 'https://rent.pe.ntu.edu.tw/'
         self.weather_url = weather_url
         self.count_dict = {}
-        
+
     def getCrowdMeterDF(self):
         self.fetchCrowdMeterNow()
         self.fetchWeatherNow()
@@ -22,7 +23,6 @@ class scraper(object):
         # build dataframe
         tmp = pd.DataFrame.from_dict([self.count_dict])
         tmp = tmp.set_index("Timestamp")
-        # tmp.to_csv(f"{base_path}/RSF_tmp.csv")
         if tmp.shape[0] == 0:
             print("NO DATA, EXIT PROGRAM")
             sys.exit()
@@ -31,20 +31,27 @@ class scraper(object):
 
     def fetchCrowdMeterNow(self):
         res = requests.get(self.url)
-        print(res)
-        count_dict = json.loads(res.text)
-        count_dict = count_dict["CounterData"][0]
-        current_count = int(count_dict['innerCount'].split(";")[0])
-        current_count_swim = int(count_dict['innerCount'].split(";")[1])
-        capacity_full = int(count_dict['permitNum'].split(";")[0])
-        capacity_full_swim = int(count_dict['permitNum'].split(";")[1])
+        soup = BeautifulSoup(res.content, 'html.parser')
+        
+        fitness_center = soup.find_all("div", class_="CMCItem")[0]
+        swimming_pool = soup.find_all("div", class_="CMCItem")[1]
+
+        current_count_fc = int(fitness_center.find_all("span")[0].text)
+        optimal_count_fc = int(fitness_center.find_all("span")[1].text)
+        max_count_fc = int(fitness_center.find_all("span")[2].text)
+
+        current_count_sp = int(swimming_pool.find_all("span")[0].text)
+        optimal_count_sp = int(swimming_pool.find_all("span")[1].text)
+        max_count_sp = int(swimming_pool.find_all("span")[2].text)
 
         # build dictionary
-        self.count_dict["current_count"] = current_count
-        self.count_dict["capacity_fulll"] = capacity_full
-        self.count_dict["capacity_ratio"] = round(current_count / capacity_full, 4)
-        self.count_dict["current_count_swim"] = current_count_swim
-        self.count_dict["capacity_full_swim"] = capacity_full_swim
+        self.count_dict["current_count"] = current_count_fc
+        # self.count_dict["optimal_count_fc"] = optimal_count_fc
+        self.count_dict["capacity_fulll"] = max_count_fc
+        self.count_dict["capacity_ratio"] = round(current_count_fc / max_count_fc, 4)
+        self.count_dict["current_count_swim"] = current_count_sp
+        # self.count_dict["optimal_count_sp"] = optimal_count_sp
+        self.count_dict["capacity_full_swim"] = max_count_sp
         self.count_dict["Timestamp"] = datetime.datetime.today().strftime("%Y-%m-%d %H:%M")
 
     def fetchWeatherNow(self):
@@ -57,3 +64,4 @@ class scraper(object):
         self.count_dict["temp_max"] = weather_res["main"]["temp_max"]
         self.count_dict["pressure"] = weather_res["main"]["pressure"]
         self.count_dict["humidity"] = weather_res["main"]["humidity"]
+
